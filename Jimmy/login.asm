eeacf89ffaccd119c82ae61083637e64e90d36d8
Jimmy Chan <jimmychankahlok66@gmail.com>
2023-09-03 18:49:41 +0800

    85%

--- Jimmy/login.asm
+++ Jimmy/login.asm
@@ -17,12 +17,12 @@ userId_Msg db 10,13,"Username (x = exit): $"
 pw_Msg db 10,13,"Password (x = exit): $"
 id db "sportxpert$"
 pw db "password$"
-invalidMemberIdMsg db 10,13,"Invalid Member ID."
+invalidMemberIdMsg db 10,13,"Invalid Member ID.$"
 memberProceedMsg db 10,13,"Do you want check membership (y) or back to asking membership (n) > $"
 memberid_Arr db "B01,B02,S01,S03,S05,G02,G03,G04,B03,$"
 memberid_Arr2 db 30 dup ('$')
-hasMembershipMsg db 10,13,"Do customer has a membership (y OR n): $"
-invalidCharMsg db 10,13,"Please enter a valid character (y OR n).",'$'
+hasMembershipMsg db 10,13,"Do customer has a membership (y = yes , n = no): $"
+invalidCharMsg db 10,13,"Please enter a valid character (y = yes , n = no).",'$'
 pressAnytoContinue db 10,13,"Press any key to continue...$"
 ReadMemberIDMsg db 10,13,10,13,"Enter Member ID : $"
 correctMemberId db 0
@@ -32,19 +32,31 @@ MembershipRate dw ?
 BronzeMsg db 10,13,"This customer is Bronze Membership.  Customer can enjoy 5% discount.$"
 SilverMsg db 10,13,"This customer is Silver Membership.  Customer can enjoy 10% discount.$"
 GoldMsg db 10,13,"This customer is Gold Membership.  Customer can enjoy 15% discount.$"
+isDot dw 0
+ErrorFound dw 0
+InvalidAmountPaidMsg db 10,13,"Only Accept number (1~9) and and dot (.) $"
 
+AmountPaidCount dw ?
+AmountPaidtemp dw ?
+exceedDecimal db 0
+ReenterAMount db 10,13,"Only accept 2 Decimal Places! Please re-enter again the amount.$"
+whetherGotDecimal db ?
 
+grandTotalMsg db "Grand Total                 = RM $"
+SSTMsg db "(+) SST (10%)               = RM $"
+DiscountAmount db "(-) Membership Discount $"
+equalSign db " = RM $"
+NetTotalToPay db "Amount To Paid              = RM $"
+EnterAmount db 10,13,"Enter Amount                = RM $"
 
-grandTotalMsg db "Grand Total             = RM $"
-SSTMsg db "(+) SST (10%)           = RM $"
-DiscountAmount db "(-) Membership Discount = RM $"
-NetTotalToPay db "NET TOTAL               = RM $"
-EnterAmount db "Enter Amount = RM$"
+AmountPaid dw ?
+AmountPaidDecimal12 dw ?
+
 
 SST dw 10
 tempDiscounted dw 0
-grandTotal dw 1
-grandTotalDecimal12 dw 55
+grandTotal dw 13107
+grandTotalDecimal12 dw 99
 grandTotalDecimal34 dw 0
 
 NetTotal dw 0
@@ -91,6 +103,11 @@ pwArr LABEL BYTE
 MAX_pwArr DB 30 
 ACT_pwArr DB ? 
 input_pwArr DB 30 DUP ('$')
+
+PaidAmount label byte
+    max_PaidAmount db 10
+    act_PaidAmount db ?
+    PaidAmount_Arr db 10 dup('$')
 .code
 main proc
 mov ax,@data
@@ -99,30 +116,181 @@ mov ds,ax
 call cls
 call login
 call paymentFUNCTION
-call cls
-print Paymentlogo
-print headline 
-print processingPaymentMsg
-print headline
-print newline
-print grandTotalMsg
-call printoutGrandTotalFUNCTION
-print newline
-print SSTMsg
-call printoutSSTFUNCTION
-print newline
-print DiscountAmount
-call printoutDiscountedTotalFUNCTION
-print newline
-print NetTotalToPay
-call printoutNetTotalFUNCTION
-
-
+printAllAmountAgain:
+call printoutAllAmount
+mov AmountPaid,0
+mov AmountPaidDecimal12,0
+mov ErrorFound,0
+mov isDot,0
+call askingAmountPaidFUNCTION
+mov ax,ErrorFound
+cmp ax,1
+je printAllAmountAgain
 
 mov ah,4ch
 int 21h
 main endp
 
+askingAmountPaidFUNCTION proc
+
+print EnterAmount
+lea dx,PaidAmount                  ;Load the AmountPaid array address to capture string input11111
+mov ah,0ah
+int 21h
+
+call whetherAmoundPaidIsNum
+mov ax,ErrorFound
+cmp ax,1
+je endingAskingAmountPaid
+call readAmountWhetherGotDecimal      ;Function that read the AmountPaid and array length11111111
+mov al,whetherGotDecimal
+cmp al,'n'
+je AmountNoDecimal
+AmountGotDecimalLabel:
+call AmountGotDecimal                ;IF got floating point then run this111111111
+mov ax,ErrorFound
+cmp ax,1
+je endingAskingAmountPaid
+jmp endingAskingAmountPaid 
+
+AmountNoDecimal:                      ;IF no floating point then run this1111111111
+call storingAmountPaidWholeNum
+endingAskingAmountPaid:
+ret
+askingAmountPaidFUNCTION endp
+whetherAmoundPaidIsNum proc
+call clear
+mov si,0
+mov cx,1
+
+validateTheStringWhetherisNum:
+mov al,[PaidAmount_Arr + si]
+cmp al,13
+je ItisvalidStringNum
+cmp al,'0'
+jl mightHaveError
+cmp al,'9'
+jg mightHaveError
+NotError:
+inc cx
+inc si
+loop validateTheStringWhetherisNum
+
+mightHaveError:
+cmp al,'.'
+jne isNotDotLabel
+inc isDot
+mov ax,isDot
+cmp ax,1
+jle  NotError
+isNotDotLabel:
+inc ErrorFound
+print InvalidAmountPaidMsg
+print pressAnytoContinue
+call pause
+
+ItisvalidStringNum:
+ret
+whetherAmoundPaidIsNum endp
+storingAmountPaidWholeNum proc
+call clear
+mov si,0
+calculationAmountPaid:
+dec AmountPaidCount
+mov cx,AmountPaidCount
+xor ah,ah
+mov al,[PaidAmount_Arr+si]     ;----multiply all the number in array into the whole number and store them into a AmountPaid variable
+inc si
+cmp AmountPaidCount,0
+je addition
+multiplication:
+mov bx,10d
+mul bx
+loop multiplication
+addition:
+add AmountPaid,ax
+cmp AmountPaidCount,0
+jnz calculationAmountPaid
+ret
+storingAmountPaidWholeNum endp
+AmountGotDecimal proc
+call storingAmountPaidWholeNum
+mov AmountPaidCount ,0
+call clear
+mov cx,1
+inc AmountPaidtemp
+mov si,AmountPaidtemp
+mov exceedDecimal,0
+Reading_AmoundPaidFloatingNumber:     ; --Reading digit Place and store back that converted character into array
+mov al,exceedDecimal
+cmp al,3
+jge exceddDecimalLabel
+mov al,[PaidAmount_Arr+si]
+cmp al,13
+je next2
+sub al,30h
+mov [PaidAmount_Arr+si],al
+inc si
+inc cx
+inc exceedDecimal
+inc AmountPaidCount
+loop Reading_AmoundPaidFloatingNumber
+
+exceddDecimalLabel:
+print ReenterAMount
+print pressAnytoContinue
+call pause
+inc ErrorFound
+jmp endingstoringAmountWholeNum
+next2:
+call clear
+mov si,AmountPaidtemp
+calculationAmountPaidF:
+dec AmountPaidCount
+mov cx,AmountPaidCount
+xor ah,ah
+mov al,[PaidAmount_Arr+si]     ;----multiply all the number in array into the real number and store them into a new variable
+inc si
+cmp AmountPaidCount,0
+je additionF
+multiplicationF:
+mov bx,10d
+mul bx
+loop multiplicationF
+additionF:
+add AmountPaidDecimal12,ax
+cmp AmountPaidCount,0
+jnz calculationAmountPaidF
+endingstoringAmountWholeNum:
+ret
+AmountGotDecimal endp
+readAmountWhetherGotDecimal proc
+mov AmountPaidCount ,0
+call clear
+mov cx,1
+mov si,0
+Reading_priceWholeNumber:     ; this label is to read array length and subtract the 30hex  and store back the number into array FOR STORING THE DECIMAL NOT ASCII
+mov al,[PaidAmount_Arr+si]
+mov AmountPaidtemp,si
+cmp al,'.'
+je next1
+cmp al,13
+je AmountNoDecimalLabel
+sub al,30h
+mov [PaidAmount_Arr+si],al
+inc si
+inc cx
+inc AmountPaidCount
+loop Reading_priceWholeNumber
+next1:
+mov whetherGotDecimal,'y'
+jmp endingReadAmountWhether
+AmountNoDecimalLabel:
+mov whetherGotDecimal,'n'
+jmp endingReadAmountWhether
+endingReadAmountWhether:
+ret
+readAmountWhetherGotDecimal endp
 paymentFUNCTION proc
 call hasMembership
 mov al,gotMembership
@@ -250,43 +418,6 @@ jmp lp3
 endPrintoutNetTotal:
 ret
 printoutNetTotalFUNCTION endp
-printoutDiscountedTotalFUNCTION proc
-    call clear
-    mov ax,NetTotal
-    call converter
-    mov ah,02h
-    mov dl,'.'
-    int 21h
-    call clear
-    mov ax,NetTotalDecimal12
-    cmp ax,10
-    jl printing12For0
-    backPrinting12:
-    call clear
-    mov ax,NetTotalDecimal12
-    call converter
-    mov ax,NetTotalDecimal34
-    cmp ax,10
-    jl printing34For0
-    backPrinting34:
-    call clear
-    mov ax,NetTotalDecimal34
-    call converter
-    jmp endprintoutDiscount
-    printing34For0:
-    mov ah,02h
-    mov dl,'0'
-    int 21h
-    jmp backPrinting34
-    
-    printing12For0:
-    mov ah,02h
-    mov dl,'0'
-    int 21h
-    jmp backPrinting12
-    endprintoutDiscount:
-    ret
-printoutDiscountedTotalFUNCTION endp
 NetTotalFUCTION proc
 mov ax, NetTotal
 add ax, SSTGrandTotal
@@ -458,18 +589,7 @@ call cls
 jmp MembershipProceedLabel  
 
 ;--------------------------------------------------------------------------------------------------
-Bronze:
- mov MembershipRate,5
-print BronzeMsg
- jmp exitMembership
- Silver:
- mov MembershipRate,10
- print SilverMsg
- jmp exitMembership
- Gold:
- mov MembershipRate,15
- print GoldMsg
- jmp exitMembership
+
 checkMembership:        ; check member id whether it is valid
 call ReadMembershipF        ;Read Input of membership   
 call validateMemberF        ; Validate Membership Function
@@ -491,7 +611,24 @@ cmp al,'S'
 je Silver
 cmp al,'G'
 je Gold
-
+Bronze:
+ mov MembershipRate,5
+print BronzeMsg
+print pressAnytoContinue
+call pause
+ jmp exitMembership
+ Silver:
+ mov MembershipRate,10
+ print SilverMsg
+print pressAnytoContinue
+call pause
+ jmp exitMembership
+ Gold:
+ mov MembershipRate,15
+ print GoldMsg
+ print pressAnytoContinue
+call pause
+ jmp exitMembership
 exitMembership:
 ret
 hasMembership endp
@@ -597,6 +734,85 @@ endforsorting:
     endingvalidateMemberF:
     ret
 validateMemberF endp
+printoutDiscountedTotalFUNCTION proc
+    call clear
+    mov ax,grandTotalDiscounted
+    call converter
+    mov ah,02h
+    mov dl,'.'
+    int 21h
+    call clear
+    mov ax,Decimal12Discounted
+    cmp ax,10
+    jl printing12For0
+    backPrinting12:
+    call clear
+    mov ax,Decimal12Discounted
+    call converter
+    mov ax,Decimal34Discounted
+    cmp ax,10
+    jl printing34For0
+    backPrinting34:
+    call clear
+    mov ax,Decimal34Discounted
+    call converter
+    jmp endprintoutDiscount
+    printing34For0:
+    mov ah,02h
+    mov dl,'0'
+    int 21h
+    jmp backPrinting34
+    
+    printing12For0:
+    mov ah,02h
+    mov dl,'0'
+    int 21h
+    jmp backPrinting12
+    endprintoutDiscount:
+ret
+printoutDiscountedTotalFUNCTION endp
+printoutAllAmount proc
+    call cls
+    print Paymentlogo
+    print headline 
+    print processingPaymentMsg
+    print headline
+    print newline
+    print grandTotalMsg
+    call printoutGrandTotalFUNCTION
+    print newline
+    print SSTMsg
+    call printoutSSTFUNCTION
+    mov al,gotMembership
+    cmp al,'n'
+    je skipDisplayDiscountAmount
+    print newline
+    print DiscountAmount
+    mov ax,MembershipRate
+    call converter
+    mov dl,'%'
+    mov ah,02h
+    int 21h
+    mov ax,MembershipRate
+    cmp al,5
+    je printSpace
+    backToprintingEqual:
+    print equalSign
+    call printoutDiscountedTotalFUNCTION
+    skipDisplayDiscountAmount:
+    print headline
+    print newline
+    print NetTotalToPay
+    call printoutNetTotalFUNCTION
+    jmp endingprintAllAmount
+    printSpace:
+    mov dl,32
+    mov ah,02h
+    int 21h
+    jmp backToprintingEqual
+    endingprintAllAmount:
+    ret
+printoutAllAmount endp
 login proc
     username:
     print logo
